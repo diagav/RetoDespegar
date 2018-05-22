09f33701163e5877eb5e266ab9b3be938832cfc1
Diana Gaviria <DIAGAVIR@pb0b0817353.bancolombia.corp>
2018-05-22 13:41:57 -0500

    Prueba

--- src/main/java/runners/Runner.java
+++ src/main/java/runners/Runner.java
@@ -8,7 +8,6 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="ProjectFeatures", glue="stepDefinitions", tags= {"@busqueda", "~@sinfecharegreso", "~@siningresardestino"})
 //@CucumberOptions(features="ProjectFeatures", glue="stepDefinitions")
-//@CucumberOptions(features="ProjectFeatures", glue="stepDefinitions", tags= {"@vuelos, ~@busqueda, ~@sinfecharegreso, @siningresardestino"})
 public class Runner {
 	
 }
